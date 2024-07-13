dd0555ea1407f6895f2e3af7e5f3a909690b8778
Lukaos187 <lukas.jensen@web.de>
2024-07-13 18:42:43 +0200

    ADD some files for upcoming translation options

---
+++ src/main/java/me/lukaos187/warpsandhomes/util/Translator.java
@@ -0,0 +1,12 @@
+package me.lukaos187.warpsandhomes.util;
+
+import org.bukkit.entity.Player;
+
+public class Translator {
+    public void translate(Player recipient, String ... messages){
+
+        //TODO code this..
+
+
+    }
+}
